d6db49f086f8c9f2aa06ba78ff467148e2638618
Dmytro Hushchin <dhuchchin@gmail.com>
2022-07-04 12:27:16 +0300

    Start page update

--- start.py
+++ start.py
@@ -18,29 +18,47 @@ st.markdown("# Model Monitoring. " + ("Local" if args.local else "DB") + " data.
 
 st.markdown("## Welcome to the Start Page!")
 
-# add radio buttons for selection which data would be used in the future
-data_mode = st.radio("Please select data", ("Standard", "Upload from .csv"))
+col1, col2, col3 = st.columns(3)
 
-# add data upoading section
-if data_mode == "Upload from .csv":
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+# add radio buttons for selection which data would be used in the future
+data_mode = col1.radio("Please select data", ("Standard", "Upload from .csv"))
+
+try:
+    # add data upoading section
+    if data_mode == "Upload from .csv":
+        data_encoding = col2.radio("Please select encoding", ("utf-8", "cp1252"))
+        uploaded_file = st.file_uploader("Choose a file")
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file, encoding=data_encoding)
+        else:
+            df = pd.DataFrame()
+            
+        container = col3.container()
+        all = col3.checkbox("Select all")
+    
+        if all:
+            options = df.columns
+        else:
+            options = container.multiselect('Choose columns', df.columns)
+            
+        df = df.loc[:, options]
+        # df.drop(columns=options, inplace=True)
     else:
-        df = pd.DataFrame()
-else:
-    df = pd.read_csv(os.path.join(DATA_PATH, "train.csv"), encoding="cp1252", 
-                     usecols=['accommodates', 'bathrooms', 'bedrooms', 'beds', 
-                              'latitude', 'longitude', 'review_scores_rating'])
-    df.bathrooms = df.bathrooms.astype(int)
-    df.bedrooms = df.bedrooms.astype(int)
-    df.beds = df.beds.astype(int)
-    df.review_scores_rating = df.review_scores_rating.astype(int)
+        df = pd.read_csv(os.path.join(DATA_PATH, "train.csv"), encoding="cp1252", 
+                        usecols=['accommodates', 'bathrooms', 'bedrooms', 'beds', 
+                                'latitude', 'longitude', 'review_scores_rating'])
+        df.bathrooms = df.bathrooms.astype(int)
+        df.bedrooms = df.bedrooms.astype(int)
+        df.beds = df.beds.astype(int)
+        df.review_scores_rating = df.review_scores_rating.astype(int)
+except:
+    st.error('## Sorry, something went wrong!')
+    st.stop()
     
 
 # display uploaded file
 if not df.empty:
-    st.table(df.head(5).append(df.tail(5)))
+    st.table(pd.concat([df.head(5), df.tail(5)]))
 
 # load data
 # df_metrics = pd.read_csv(os.path.join(DATA_PATH, 'metrics.csv'))
